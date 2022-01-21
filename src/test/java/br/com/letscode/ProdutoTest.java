8ccfe8a8941e9fa8697ab2390fe49b1eb5df595c
Felipe Luiz <felipe.andrade33@etec.sp.gov.br>
2022-01-21 19:21:24 -0300

    Initial commit

---
+++ src/test/java/br/com/letscode/ProdutoTest.java
@@ -0,0 +1,11 @@
+package br.com.letscode;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ProdutoTest {
+//    @Test
+//    public void primeiroTeste(){
+//        Assert.fail("Não compilado");
+//    }
+}
